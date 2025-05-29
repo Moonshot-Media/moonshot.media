4dc597eab440e8bcff74d5de9f943932394f4daf
The Nguyen <nmthe.net@gmail.com>
2025-05-29 16:00:17 +0700

    Project Section [Done]

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,6 @@
 function Card(props: any) {
     return (
-        <div className="relative w-1/3 flex flex-col bg-red-50 rounded-lg">
+        <div className="relative flex flex-col bg-red-50 rounded-lg w-full md:flex-[0_1_calc(50%-1rem)] lg:flex-[0_1_calc(33.333%-1rem)]">
             <div className="p-4">
                 <div className="w-10">
                     {props.icon}
@@ -9,9 +9,12 @@ function Card(props: any) {
                     {props.title}
                 </h5>
             </div>
-            <div className="mx-3 border-t border-slate-200 pb-3 pt-2 px-1">
+            <div className="mx-3 border-t border-zinc-200 pb-3 pt-2 px-1 flex justify-between">
                 <span className="text-sm text-slate-600 font-medium">
-                    Last updated: 4 hours ago
+                    <img src={props.flag} className="w-5"/>
+                </span>
+                <span className="text-sm text-slate-600 font-medium">
+                    Since {props.year}
                 </span>
             </div>
         </div>
