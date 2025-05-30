0da891e470decbf53eaff88b0efa8363414f99ff
The Nguyen <nmthe.net@gmail.com>
2025-05-30 11:21:12 +0700

    Contact section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className="relative mx-auto w-full pt-30">
+        <footer className="relative mx-auto w-full pt-10">
             <div className="w-full max-w-5xl mx-auto text-center text-gray-700 py-5 border-t border-zinc-100">
                 © {new Date().getFullYear()} Moonshot Media S.L.U.
             </div>
--- src/sections/Jobs.tsx
+++ src/sections/Jobs.tsx
@@ -1,7 +1,7 @@
 function Jobs() {
     return (
         <div className="relative mx-auto w-full pt-20 px-3 md:px-5 lg:px-8">
-            <div className="w-full max-w-5xl mx-auto mb-10" id="jobs">
+            <div className="w-full max-w-5xl mx-auto mb-5" id="jobs">
                 <h1 className="text-3xl lg:text-3xl font-bold text-zinc-800">Jobs</h1>
             </div>
             <div className="w-full max-w-5xl mx-auto">
